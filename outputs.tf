cb0937eb7a10a331c82f4d0a472fc726fd8ee493
HadassahGottfried <Hadasa3281@gmail.com>
2025-02-05 20:38:36 +0200

    Unit B

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_ip" {
-  description = "The external IP of the instance"
-  value       = google_compute_instance.example.network_interface[0].access_config[0].nat_ip
-}
--- terraform.tfvars
+++
@@ -1,4 +0,0 @@
-project      = "terraform-chany-wexelshtein"
-region       = "me-west1"
-zone         = "me-west1-b"
-machine_type = "e2-micro"
